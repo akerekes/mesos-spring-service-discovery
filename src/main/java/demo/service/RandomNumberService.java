a0e8e95ceb6912bfaf473b3b7a2ec44190fbf99b
andras.kerekes <andras.kerekes.work@gmail.com>
2015-07-21 15:13:36 -0400

    logging added to RandomNumberService

--- src/main/java/demo/service/RandomNumberService.java
+++ src/main/java/demo/service/RandomNumberService.java
@@ -2,12 +2,17 @@ package demo.service;
 
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RandomNumberService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(RandomNumberService.class);
+
 	public int getRandomNumber() {
+		LOGGER.info("Random number requested");
 		return new Random().nextInt(100);
 	}
 }
